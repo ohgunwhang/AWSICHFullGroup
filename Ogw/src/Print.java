8fd4e77a16b9a67c8c4c138f94b16409c0a1f8c4
EZEN-131 <EZEN-131@DESKTOP-P1Q6O7B>
2022-07-13 15:50:42 +0900

    인원
    Signed-off-by: EZEN-131 <EZEN-131@DESKTOP-P1Q6O7B>

---
+++ Ogw/src/Print.java
@@ -0,0 +1,10 @@
+
+public class Print {
+	public static void main(String[] args) {
+		System.out.println("KJM KSJ KTH");
+		System.out.println("NKH PYM SDY");
+		System.out.println("ABK KJS LDH");
+		System.out.println("JHS HWJ CEJ");
+	}
+
+}
